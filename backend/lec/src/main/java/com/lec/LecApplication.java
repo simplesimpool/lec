2462095c2e969f464002ee92975588721a54024f
simplesimpool <bojiwajaji66@gmail.com>
2025-11-09 22:10:04 +0900

    initial commit

---
+++ backend/lec/src/main/java/com/lec/LecApplication.java
@@ -0,0 +1,13 @@
+package com.lec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecApplication.class, args);
+	}
+
+}
