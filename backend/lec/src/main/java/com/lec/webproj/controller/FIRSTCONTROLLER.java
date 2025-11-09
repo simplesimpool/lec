159af1c181ab5018fd746e4db54cfac1e693085e
simplesimpool <bojiwajaji66@gmail.com>
2025-11-09 22:35:36 +0900

    commit

---
+++ backend/lec/src/main/java/com/lec/webproj/controller/FIRSTCONTROLLER.java
@@ -0,0 +1,5 @@
+package com.lec.webproj.controller;
+
+public class FIRSTCONTROLLER {
+
+}
---
+++ backend/lec/src/test/java/com/lec/webproj/LecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lec.webproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
